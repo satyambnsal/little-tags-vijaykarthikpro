ec635708a04e9d409ee37bc9bc157ecec3cc634b
Rasika Hatwar <rasikahatwar13@gmail.com>
2021-04-09 16:41:07 +0530

    Add header UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import * as ROUTES from "./constants/routes";
+import AccountPage from "./components/AccountPage/AccountPage";
+import ProductsListPage from "./components/ProductsListPage/ProductListPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <HomePage />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path={ROUTES.LANDING}>
+            <HomePage />
+          </Route>
+          <Route path={ROUTES.HOME}>
+            <HomePage />
+          </Route>
+          <Route path={ROUTES.ACCOUNT}>
+            <AccountPage />
+          </Route>
+          <Route path={ROUTES.OFFERS}>
+            <ProductsListPage />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import Dropdown from "./Dropdown/Dropdown";
+import AppLogo from "../../assets/applogo.png";
 
 export default function Header() {
-  return <div>Header</div>;
+  const [dropdown, setDropdown] = useState(false);
+  const onMouseHover = () => {
+    setDropdown(true);
+  };
+  const onMouseLeave = () => {
+    setDropdown(false);
+  };
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={AppLogo} alt="" width="50px" height="50px" />
+        <span className="title">StyleBeast</span>
+      </div>
+
+      <ul className="nav-category">
+        <li className="nav-item">
+          <Link to="/" className="nav-links">
+            Men
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-links">
+            Women
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-links">
+            Offers
+          </Link>
+        </li>
+      </ul>
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <Link to="/" className="nav-links">
+            Search
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-links">
+            Language Support
+          </Link>
+        </li>
+        <li
+          className="nav-item"
+          onMouseEnter={onMouseHover}
+          onMouseLeave={onMouseLeave}
+        >
+          <Link to="/" className="nav-links">
+            Profile
+          </Link>
+          {dropdown && <Dropdown />}
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-links">
+            Wishlist
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-links">
+            Cart
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
