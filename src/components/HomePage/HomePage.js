a6b58806045afaf87ee9b31b1680f1efe1a03844
Vijay Karthik <vijaykarthikpro@gmail.com>
2021-04-10 01:53:24 +0530

    Add home page UI

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,53 @@
 import React from "react";
+import './HomePage.scss'
+import HomeBanner from '../../assets/images/home-page-banner.svg'
+import MenPortrait from '../../assets/images/men-portrait.svg'
+import WomenPortrait from '../../assets/images/women-portrait.svg'
+import JacketsImg from '../../assets/images/jackets.svg'
+import TshirtsImg from '../../assets/images/tshirts.svg'
+import ElectronicsImg from '../../assets/images/electronics.svg'
+import JeweleryImg from '../../assets/images/jewelery.svg'
 
 export default function HomePage() {
-  return <div>Sections</div>;
+  return (<div className="home-container">
+    <div className="banner-section">
+      <img className="banner" src={HomeBanner} alt="img"/>
+    </div>
+    <div className="section-1">
+      <h1>COLLECTIONS</h1>
+      <div className="collections">
+        <div className="men">
+          <h2>MEN</h2>
+          <img src={MenPortrait} alt="img"/>
+        </div>
+        <p>"Fashion changes, but<br/>style endures"</p>
+        <div className="women">
+          <h2>WOMEN</h2>
+          <img src={WomenPortrait} alt="img"/>
+        </div> 
+      </div>
+    </div>
+    <div className="section-2">
+      <h1>CATEGORIES TO ADD</h1>
+      <div className="categories-to-add">
+        <div>
+          <img src={JacketsImg} alt="img"/>
+          <h2>Jackets</h2>
+        </div>
+        <div>
+          <img src={TshirtsImg} alt="img"/>
+          <h2>T-shirts</h2>
+        </div>
+        <div>
+          <img src={ElectronicsImg} alt="img"/>
+          <h2>Electronics</h2>
+        </div>
+        <div>
+          <img src={JeweleryImg} alt="img"/>
+          <h2>Jewelery</h2>
+        </div>
+
+      </div>
+    </div>
+    </div>);
 }
