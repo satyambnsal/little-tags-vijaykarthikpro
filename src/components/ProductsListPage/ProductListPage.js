8b77b26ec2ee9b49506165a7ccef842b968096bb
Rasika Hatwar <rasikahatwar13@gmail.com>
2021-04-11 03:07:41 +0530

    Add product card ui

--- src/components/ProductsListPage/ProductListPage.js
+++ src/components/ProductsListPage/ProductListPage.js
@@ -1,5 +1,23 @@
 import React from "react";
+import products from "../../data/products";
+import "./ProductListPage.scss";
 
-export default function ProductListPage() {
-  return <div>ProductListPage</div>;
-}
+const MainContent = () => {
+  const listItems = products.map((item) => (
+    <div className="card" key={item.id}>
+      <div className="card_img">
+        <img src={item.image} alt="" />
+      </div>
+      <div className="card_header">
+        <h2>{item.title}</h2>
+        <p className="price">
+          <span>&#8377;</span>
+          {item.price}
+        </p>
+        <div className="btn">Add to wishlist</div>
+      </div>
+    </div>
+  ));
+  return <div className="main_content">{listItems}</div>;
+};
+export default MainContent;
