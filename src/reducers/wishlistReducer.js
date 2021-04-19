3e9ad883579419985db61b3458226cd23e99db56
Vijay Karthik <vijaykarthikpro@gmail.com>
2021-04-19 18:52:08 +0530

    Add cart, wishlist, order reducers with actions

---
+++ src/reducers/wishlistReducer.js
@@ -0,0 +1,34 @@
+import * as ACTIONS from '../constants/actionTypes';
+
+const initialState = {
+    wishlist: []
+}
+
+export default function cartReducer(state = initialState, action) {
+    
+    switch(action.type) {
+        case ACTIONS.ADD_ITEM_TO_WISHLIST : {
+            const { product } = action;
+            return { ...state, wishlist : [...state.wishlist, product]};
+        }
+        case ACTIONS.REMOVE_ITEM_FROM_WISHLIST : {
+            const { wishlist } = state;
+            const { product: { id }} = action
+            const filteredWishlist = wishlist.map((product) => product.id !== id);
+            return { ...state, wishlist : filteredWishlist} 
+        }
+        case ACTIONS.UPDATE_WISHLIST_ITEM : {
+            const { wishlist } = state;
+            const { id, size, quantity } = action
+            wishlist.map((product) => {
+                if(product.id === id) {
+                    return { ...product, size, quantity }
+                }
+                return product;
+            });
+            break;
+        }
+        default: 
+            return state;
+    }
+}
