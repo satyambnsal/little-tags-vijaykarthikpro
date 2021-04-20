89690f911be76c179951a5e95a64b9cfe49c2957
Vijay Karthik <vijaykarthikpro@gmail.com>
2021-04-20 19:19:23 +0530

    Fix authorization html code

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import FirebaseContext from "../Firebase/context";
 import * as ROUTES from "../../constants/routes";
+import './withAuthorization.scss';
 
 const withAuthorization = (Component) => {
   const NewComponent = (props) => {
@@ -13,16 +14,16 @@ const withAuthorization = (Component) => {
         props.history.push(ROUTES.HOME);
       }
     };
-    const fallback = () => /* props.history.push(ROUTES.HOME) */{};
+    const fallback = () => {};
     useEffect(() => {
       firebase.onAuthChangeListener(next, fallback);
     });
 
     return props.authUser ? (
       <Component {...props} />
-    ) : (
-      <p>You need to sign in to access this page </p>
-      
+    ) : (<div className="authorization-container">
+        <p>You need to login in to access this page</p>
+    </div>
     );
   };
 
